c3e12f1f08b015afb11769e0fb6377cee036b368
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-21 07:01:28 +0900

    add lnapsack code

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -4,18 +4,61 @@ import java.util.stream.Collectors;
 
 class Knapsack {
 
-    /**
-     * リストを指定価格以下にフィルタする
-     * @param mealDataList 用いるリスト, MealData型のみ対応
-     * @param maxValue フィルタしたい価格
-     * @return 指定価格以下にフィルタされたリスト
-     */
-    public static List<MealData> filterVal(final List<MealData> mealDataList, final int maxValue) {
-        final List<MealData> filterList =
-                mealDataList.stream()
-                        .filter(mealData -> mealData.getValue() <= maxValue)
-                        .collect(Collectors.toList());
-        return filterList;
+    static List<Integer> knapsack(int num, int Money, int[] cost, int[] weight){
+        int[][] dp = new int[num + 1][Money + 1];
+        int[][] prev_w = new int[num + 1][Money + 1];
+        List<Integer> res = new ArrayList<Integer>();
+
+        for (int i = 0; i < num; ++i) {
+            for (int j = 0; j <= Money; ++j) {
+    
+                // i 番目の品物を選ぶ場合
+                if (j >= cost[i]) {
+                    if (dp[i + 1][j] < dp[i][j - cost[i] ] + weight[i]) {
+                        dp[i + 1][j] = dp[i][j - cost[i] ] + weight[i];
+                        prev_w[i + 1][j] = j - cost[i];
+                    }
+                }
+    
+                // i 番目の品物を選ばない場合
+                if (dp[i + 1][j] < dp[i][j]) {
+                    dp[i + 1][j] = dp[i][j];
+                    prev_w[i + 1][j] = j;
+                }
+            }
+        }
+        // 復元
+        int cur_w = Money;
+        for (int i = num - 1; i >= 0; --i) {
+            // 選んでいた場合
+            if (prev_w[i + 1][cur_w] == cur_w - cost[i]) {
+                // System.out.println( i + " th item (cost = " + cost[i] + ", weight = " + weight[i] + ")");
+                res.add(i);
+            }
+
+            // 復元テーブルをたどる
+            cur_w = prev_w[i + 1][cur_w];
+        }
+        // 適切に選んだときの金額はdp[num][Money]
+        return res;
+    }
+
+    public static List<Integer> filterVal(final List<MealData> mealDataList, final int Money) {
+        
+        int[] weight = new int[mealDataList.size()];
+        int[] value = new int[mealDataList.size()];
+
+        int pos = 0;
+        for (MealData data : mealDataList) {
+            weight[pos] = 1;
+            value[pos] = data.getValue();
+            pos += 1;
+        }
+        List<Integer> res = knapsack(mealDataList.size(), Money, value, weight);
+        // for (Integer i : res) {
+        //     System.out.println(i);
+        // }
+        return res;
     }
 
 }
