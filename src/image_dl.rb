94f887cfa4cf11e77aac201dbfb6b18cf5d0ca9f
AjxLab <abe12@mccc.jp>
2020-02-01 00:37:26 +0900

    rm mdap.rb

--- src/image_dl.rb
+++ src/image_dl.rb
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
-require 'fileutils'
+eequire 'fileutils'
 require 'open-uri'
 require 'husc'
-require './src/mdap'
+require 'mdap'
 
 def webDL(link, file)
   ## -----*----- Webからダウンロード -----*----- ##
